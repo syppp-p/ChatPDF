c0f08909fc394aab76442a861d0655efd67c6aea
syppp-p <sur03211@naver.com>
2025-07-28 23:25:41 +0900

    수정

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 __import__('pysqlite3')
 import sys
-sys.modules['sqllite3'] = sys.modules.pop('pysqlite3')
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
